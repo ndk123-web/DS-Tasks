b5b6bcfb12a70325e44bee799d37979c4c8732c9
Navnath Kadam <navnathkadam284@gmail.com>
2025-09-05 22:25:14 +0530

    Added Task 8 remaining consiistency

---
+++ Task 8 ( simple )/client.mjs
@@ -0,0 +1,62 @@
+import { Client } from "ndk-rpc-engine/client";
+import readline from 'readline'
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const client = new Client({
+    server_port: 3000
+})
+
+const main = async (count) => {
+    if (count === 50) {
+        console.log("Max Limit Reached !!")
+        return;
+    }
+    rl.question("1. Automatic \n2. Manual \n3. Exit \n", async (selected) => {
+        if (selected !== '1' && selected !== '2' && selected.toLowerCase() !== 'exit') {
+            console.log("Invalid Selection ")
+            main(count++) // recursion if invalid  
+            return; // its mandatory or else down code also will executes
+        }
+
+        if (selected === '1') {
+            setInterval(async () => {
+                try {
+                    const response = await client.request({
+                        method: "signal_controller",
+                        params: {}
+                    });
+
+                    console.log("Response:", response);
+                    rl.close()
+                } catch (err) {
+                    console.error("Request failed:", err.message);
+                }
+            }, 5000);
+        }
+
+        else if (selected === "2") {
+            rl.question("Enter Number To be Green the Road\n", async (road) => {
+                if (road < 1 || road > 4) {
+                    console.log("Invalid Road Choose from (1-4)")
+                    main(count++);
+                    return;
+                }
+                const response = await client.request({
+                    method: 'manual',
+                    params: {
+                        road: road
+                    }
+                })
+                console.log("Response: ", response);
+                rl.close()
+            })
+        }
+        return; // not important but for ease just returning or else it automatically will be return
+    })
+}
+
+main(0);
---
+++ Task 8 ( simple )/server.mjs
@@ -0,0 +1,167 @@
+import ndk_rpc_server from "ndk-rpc-engine/server";
+
+const server = new ndk_rpc_server({
+  port: 3000
+})
+
+const STATUS = {
+  s12: "RED",
+  s34: "GREEN",
+  p12: "GREEN",
+  p34: "RED"
+}
+
+const selectRoad = (road) => {
+  if (road === 1 || road === 2) {
+    return [1, 2]
+  }
+  else {
+    return [3, 4]
+  }
+}
+
+const sleep = async (ms) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("STATUS: ", STATUS);
+      resolve(); // yaha resolve karna zaroori hai
+    }, ms);
+  });
+};
+
+const manual = async ({ road }) => {
+  console.log("Before Manual Status: ", STATUS)
+  console.log("Road To Be Green : ", road);
+
+  const [roadToGreen] = selectRoad(road)
+  console.log("Road to Green: ", roadToGreen)
+
+  if (roadToGreen === 1) {
+    if (STATUS.s12 === "GREEN") {
+      console.log("No Worries")
+      STATUS.s34 = "RED"
+      pedestrian_controller({ road: roadToGreen })
+      console.log("STATUS: ", STATUS)
+      await sleep(2000)
+    }
+    else {
+      STATUS.s12 = "YELLOW"
+      await sleep(2000)
+      STATUS.s12 = "GREEN"
+      STATUS.s34 = "RED"
+      pedestrian_controller({ road: roadToGreen })
+      console.log("STATUS: ", STATUS)
+      await sleep(2000)
+    }
+  }
+
+  else if (roadToGreen === 3) {
+    if (STATUS.s34 === "GREEN") {
+      console.log("No Worries")
+      STATUS.s12 = "RED"
+      pedestrian_controller({ road: roadToGreen })
+      console.log("STATUS: ", STATUS)
+      await sleep(2000)
+    }
+    else {
+      STATUS.s34 = "YELLOW"
+      await sleep(2000)
+      STATUS.s34 = "GREEN"
+      STATUS.s12 = "RED"
+
+      pedestrian_controller({ road: roadToGreen })
+
+      console.log("STATUS: ", STATUS)
+      await sleep(2000)
+    }
+  }
+  return { ...STATUS, roadToGreen: road }
+};
+
+const signal_controller = async () => {
+  const randomRoad = signal_manipulator()
+  console.log("Random Road : ", randomRoad)
+  const [road] = selectRoad(randomRoad)
+
+  if (road === 1) {
+    if (STATUS.s12 === "GREEN") {
+      console.log("No Worries")
+      STATUS.s34 = "RED"
+      STATUS.p34 = "GREEN"
+      STATUS.p12 = "RED"
+      console.log("STATUS: ", STATUS)
+      await sleep(2000)
+    }
+    else {
+      STATUS.s12 = "YELLOW"
+      await sleep(2000)
+      STATUS.s12 = "GREEN"
+      STATUS.s34 = "RED"
+      pedestrian_controller({ road: road })
+      console.log("STATUS: ", STATUS)
+      await sleep(2000)
+    }
+  }
+
+  else if (road === 3) {
+    if (STATUS.s34 === "GREEN") {
+      console.log("No Worries")
+      STATUS.s12 = "RED"
+      STATUS.p12 = "GREEN"
+      STATUS.p34 = "RED"
+      console.log("STATUS: ", STATUS)
+      await sleep(2000)
+    }
+    else {
+      STATUS.s34 = "YELLOW"
+      await sleep(2000)
+      STATUS.s34 = "GREEN"
+      STATUS.s12 = "RED"
+
+      pedestrian_controller({ road: road })
+
+      console.log("STATUS: ", STATUS)
+      await sleep(2000)
+    }
+  }
+  return { ...STATUS, roadToGreen: road }
+}
+
+// Generate Random Function 
+const signal_manipulator = () => {
+  const random = parseInt(Math.floor(Math.random() * 4) + 1)
+  return random;
+}
+
+const pedestrian_controller = ({ road }) => {
+  if (road === 1) {
+    STATUS.p12 = "GREEN"
+    STATUS.p34 = "RED"
+  }
+  else if (road === 3) {
+    STATUS.p12 = "RED"
+    STATUS.p34 = "GREEN"
+  }
+}
+
+
+await server.register_functions([
+  {
+    function_name: "signal_controller",
+    function_block: signal_controller
+  },
+  {
+    function_name: "signal_manipulator",
+    function_block: signal_manipulator
+  },
+  {
+    function_name: "pedestrian_controller",
+    function_block: pedestrian_controller
+  },
+  {
+    function_name: "manual",
+    function_block: manual
+  }
+])
+
+await server.start();
